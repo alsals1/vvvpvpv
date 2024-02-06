7ec7fcca1902a4e0886203760d215bd0ad768748
kinnbotT <gotnsla12@naver.com>
2024-02-06 10:38:00 +0900

    2월6일 수정2

--- 2월6일.py
+++ 2월6일.py
@@ -1,3 +1,15 @@
 str = "one two three"
 for i in range(len(str)):
     print(str[i]) # 빈 칸도 문자열이다.
+    # o
+    # n
+    # e
+    #
+    # t
+    # w
+    # o
+    #
+    # t
+    # h
+    # r
+    # e
